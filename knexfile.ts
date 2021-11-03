75c21aa63a1ad267811c5c407c0a5c646ac5bcc6
germangamboa95 <germangamboa95@gmail.com>
2021-11-03 01:57:50 -0400

    change env

--- knexfile.ts
+++ knexfile.ts
@@ -8,13 +8,13 @@ const migrations = {
 module.exports = {
   development: {
     client: "pg",
-    connection: process.env.DATABASE,
+    connection: process.env.DATABASE ?? process.env.DATABASE_URL,
     migrations,
   },
 
   staging: {
     client: "pg",
-    connection: process.env.DATABASE,
+    connection: process.env.DATABASE ?? process.env.DATABASE_URL,
     searchPath: ["main_schema"],
     pool: {
       min: 2,
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE,
+    connection: process.env.DATABASE ?? process.env.DATABASE_URL,
     searchPath: ["main_schema"],
     pool: {
       min: 2,
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import { Logger } from "./logger";
 import { ApiServer } from "./server";
 
 const boot = async () => {
-  const sequelize = new Sequelize(process.env.DATABASE, {
-    dialect: "postgres",
-    models: [__dirname + "/models"],
-    logging: process.env.NODE_ENV === "production" ? false : console.log,
-    define: {
-      underscored: true,
-    },
-  });
+  const sequelize = new Sequelize(
+    process.env.DATABASE ?? process.env.DATABASE_URL,
+    {
+      dialect: "postgres",
+      models: [__dirname + "/models"],
+      logging: process.env.NODE_ENV === "production" ? false : console.log,
+      define: {
+        underscored: true,
+      },
+    }
+  );
 
   console.log(sequelize.models);
   new ApiServer().start(Number(process.env.PORT));
