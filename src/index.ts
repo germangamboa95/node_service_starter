1eea54e8d2120ab9d6ac8fcd222072f8c9abc518
germangamboa95 <germangamboa95@gmail.com>
2022-02-14 00:29:07 -0500

    forogt this existed

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,18 @@ import { Logger } from "./logger";
 import { ApiServer } from "./server";
 
 const boot = async () => {
-  const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "mysql",
-    models: [__dirname + "/models"],
-    logging: process.env.NODE_ENV === "production" ? false : console.log,
-    define: {
-      underscored: true,
-    },
-  });
+  if (process.env.DATABASE_URL) {
+    const sequelize = new Sequelize(process.env.DATABASE_URL, {
+      dialect: "mysql",
+      models: [__dirname + "/models"],
+      logging: process.env.NODE_ENV === "production" ? false : console.log,
+      define: {
+        underscored: true,
+      },
+    });
+  }
 
-  console.log(sequelize.models);
-  new ApiServer().start(Number(process.env.PORT));
+  new ApiServer().start(Number(process.env.PORT ?? 3000));
 };
 
 boot().catch((err) => console.error(`Service failed to boot`, err));
